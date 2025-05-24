0c1d4d04f8908aba6848cd11f6e7f4b405c871a0
Nitish Varma <excellence@birlanu.com>
2025-05-24 15:13:20 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,46 @@
+import os 
+from pathlib import Path
+
+project_name = "US_Visa_pred"
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/pipeline/predection_pipeline.py",
+    f"{project_name}/utilis/__init__.py",
+    f"{project_name}/utilis/main_utils.py",
+    "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "config/model.yaml",
+    "config/schema.yaml"
+    ]
+
+for file_path in list_of_files:
+    filepath = Path(file_path)
+    filedir , filemame = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, 'w') as f:
+            pass
+    else:
+        print(f"File {file_path} already exists and is not empty. Skipping creation.")
