010b179774deefa9a52aac22f7a59e00fd861ab1
NitishVarma50 <nitishvarma50@gmail.com>
2025-05-24 15:50:07 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="US_Visa_pred",
+    version="0.0.1",
+    author="Nitish Varma",
+    author_email="nitishvarma50@gmail.com",
+    packages=find_packages()
+)
